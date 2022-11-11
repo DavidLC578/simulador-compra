eb5d588b7b5d2b0a0df12163a31a7e8ff30f2774
DavidLC578 <davidlatt2002@gmail.com>
2022-11-11 11:42:06 +0100

    visualizacion ambas opciones hecho

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ Opción 2: Ver todos o un usuario
 """))
 
 # Listas y/o diccionarios
-usuarios = {"david":[89143,"lattanzio"]}
+usuarios = {}
 
 while salir == False:
     match opc:
         case 1:
             print("Ha escogido registrar un usuario")
-            clave = input("Cuál es el nombre del usuario: ")
-            for valor in range(0,4):
-                valor = input("Escribe dni,apellido,pais,telefono ")
-                usuarios[clave]=[valor]
+            dni = input("Cuál es el dni del usuario: ")
+            nombre = input("Escribe el nombre: ")
+            apellido = input("Escribe el apellido: ")
+            pais = input("Escribe el pais: ")
+            telefono = input("Escribe el telefono: ")
+
+            usuarios[dni]=[nombre,apellido,pais,telefono]
             opc = int(input("""
 
 Introduce una opción: 
@@ -32,8 +35,9 @@ Opción 2: Ver un usuario
 """))
             match opc2:
                 case 1:
+                    # print(usuarios)
                     for clave,valor in usuarios.items(): 
-                        print("Nombre: ",clave,end=": ")
+                        print("DNI: ",clave,end=": ")
                         for dato in valor:
                             print(dato,end=" ")
                     opc = int(input("""
@@ -43,12 +47,18 @@ Opción 1: Registrar un usuario
 Opción 2: Ver todos o un usuario
 """))
                 case 2:
-                    usuario = input()
-                    if usuario == usuarios.keys():
+                    usuario = input("Introduce el dni de un usuario para ver sus datos: ")
+                    if usuario in usuarios:
                         for clave,valor in usuarios.items(): 
                             print("Nombre: ",clave,end=": ")
                             for dato in valor:
                                 print(dato,end=" ")
+                    opc = int(input("""
+
+Introduce una opción: 
+Opción 1: Registrar un usuario
+Opción 2: Ver todos o un usuario
+"""))
                 case _:
                     opc = int(input("""
 Introduce una opción: 
