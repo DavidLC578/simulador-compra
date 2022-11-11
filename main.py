eae2b1d6089b2bd10dea443e2c785cab8bf301a1
DavidLC578 <davidlatt2002@gmail.com>
2022-11-11 11:11:22 +0100

    fase de registro de usuarios hecho

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 salir = False
 opc = int(input("""Introduce una opción: 
 Opción 1: Registrar un usuario
+Opción 2: Ver todos o un usuario
 """))
 
 # Listas y/o diccionarios
-usuarios = {}
+usuarios = {"david":[89143,"lattanzio"]}
 
 while salir == False:
     match opc:
@@ -15,15 +16,51 @@ while salir == False:
             for valor in range(0,4):
                 valor = input("Escribe dni,apellido,pais,telefono ")
                 usuarios[clave]=[valor]
-            for clave,valor in usuarios.items():    
-                print("Nombre: ",clave,"Valores: ",valor)
+            opc = int(input("""
 
-            opc = int(input("""Introduce una opción: 
+Introduce una opción: 
 Opción 1: Registrar un usuario
+Opción 2: Ver todos o un usuario
 """))
         case 2:
-            print("2")
-            salir = True
+            # A partir de aquí se elige la opcion, ver todos los  usuarios o uno en concreto
+            opc2 = int(input("""
+
+Para ver todos o varios usuarios:
+Opción 1: Ver todos
+Opción 2: Ver un usuario
+"""))
+            match opc2:
+                case 1:
+                    for clave,valor in usuarios.items(): 
+                        print("Nombre: ",clave,end=": ")
+                        for dato in valor:
+                            print(dato,end=" ")
+                    opc = int(input("""
+
+Introduce una opción: 
+Opción 1: Registrar un usuario
+Opción 2: Ver todos o un usuario
+"""))
+                case 2:
+                    usuario = input()
+                    if usuario == usuarios.keys():
+                        for clave,valor in usuarios.items(): 
+                            print("Nombre: ",clave,end=": ")
+                            for dato in valor:
+                                print(dato,end=" ")
+                case _:
+                    opc = int(input("""
+Introduce una opción: 
+Opción 1: Registrar un usuario
+Opción 2: Ver todos o un usuario
+"""))
+                    
 
 
+# usuarios = {"wieur":[234132,"asdfasd"]}
+# for clave, valor in usuarios.items():
+#     print(clave,end=": ")
+#     for dato in valor:
+#         print(dato,end=" ")
 
